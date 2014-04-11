186347630506136e7bfae2db455ff9786837f713
BriannaZamora <bzamora@icloud.com>
2014-04-11 17:32:05 +0000

    I have done another version of rock, paper, scissors makeing our code smaller and easier,

--- simple_rps.rb
+++ simple_rps.rb
@@ -1,4 +1,10 @@
 OUTCOME = {"rock" => {"rock" => "tie", "paper" => "player 2", "scissors" => "player 1"},
-          "paper" => {},
-          "scissors" => {}
-                      }
+  "paper" => {"paper" => "tie","rock" => "player1","scissors" => "player2"},
+  "scissors" => {"scissors" => "tie","rock" => "player2","paper" => "player1"}
+  }
+
+computer_choice = ["rock", "paper", "scissors"].sample
+puts "Pick a choice: rock, paper, or scissors"
+user_choice = gets.chomp
+puts OUTCOME[user_choice][computer_choice]
+
